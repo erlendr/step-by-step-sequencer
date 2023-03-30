5eb35208527d2cb8648bdac24f7931a400dc3670
Erlend Røsjø <erlend.rosjo@spv.no>
2023-03-30 10:11:30 +0200

    pads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import playPauseButton from "./nuts-and-bolts/play-pause";
 import SimpleSynth from "./nuts-and-bolts/simple-synth";
 import bpmInput from "./nuts-and-bolts/bpm-input";
+import padInput from "./nuts-and-bolts/pad-input";
 
 function checkForWebAudio() {
   let hasWebAudio = !!window.AudioContext;
@@ -19,22 +20,29 @@ window.synth = synth;
 
 let loopStarted = 0;
 let timePassed = 0;
-let bpm = 120;
+let bpm = 150;
 
 const getBps = () => bpm / 60;
 const numberOfNotesPerBeat = 4;
+const bars = 4;
 const getBeatLength = () => 1/getBps();
 const getNoteLength = () => getBeatLength()/numberOfNotesPerBeat;
 
 let lastBeatPlayed = 0;
+let pads = Array(bars * numberOfNotesPerBeat).fill(false);
 
 function frame() {
   timePassed = audioContext.currentTime - loopStarted;
   const beatNumber = Math.floor(timePassed / getNoteLength());
 
   if(lastBeatPlayed != beatNumber) {
-    synth.play(69, audioContext.currentTime, 0.25);
     lastBeatPlayed = beatNumber;
+    let noteInGroupOf16 = beatNumber % 16
+    console.log('noteInGroupOf16', noteInGroupOf16);
+
+    if(pads[noteInGroupOf16]) {
+        synth.play(69, audioContext.currentTime, getNoteLength());
+    }
   }
 }
 
@@ -51,6 +59,7 @@ const printState = () => {
   console.log('noteLength', getNoteLength());
   console.log('bps', getBps());
   console.log('bpm', bpm);
+  console.table(pads);
 }
 
 const onPlay = () => {
@@ -63,16 +72,26 @@ const onPause = () => {
   printState();
 }
 
+const resetState = () => {
+}
+
 function init() {
   checkForWebAudio(); // You can delete this after you've checked that WebAudio is ok.
-  playPauseButton(onPlay, onPause);
   printState();
   loopStarted = audioContext.currentTime;
+  
+  
+  padInput(pads, (index) => {
+    pads[index] = !pads[index];
+    printState();
+  });
+  
   bpmInput(bpm, (x) => {
     bpm = x;
     printState();
   });
-
+  
+  playPauseButton(onPlay, onPause);
 }
 
 window.ac = audioContext;
